c26d9bce037382f0429585a41b2a403e3313a7c9
gabrielamuller <mullergabrielaa@gmail.com>
2018-09-28 13:11:17 +0100

    First commit

---
+++ race.js
@@ -0,0 +1,31 @@
+/*Here's how our registration works. There are adult runners (over 18 years of age) and youth runners (under 18 years of age). They can register early or late. Runners are assigned a race number and start time based on their age and registration.
+
+Race number:
+
+Early adults receive a race number at or above 1000.
+All others receive a number below 1000.
+Start time:
+
+Adult registrants run at 9:30 am or 11:00 am.
+Early adults run at 9:30 am.
+Late adults run at 11:00 am.
+Youth registrants run at 12:30 pm (regardless of registration).*/
+
+let raceNumber = Math.floor(Math.random() * 1000);
+
+let earlyRegistrant = false;
+
+let age = 18;
+
+if (earlyRegistrant === true && age >= 18) {
+  raceNumber += 1000;
+}
+
+if (earlyRegistrant === true && age >= 18) {
+  console.log('Runner number ' + raceNumber + ' will race at 9:30 am.')
+} else if (earlyRegistrant === false && age>= 18) {
+  console.log('Your race number is ' + raceNumber + ' and you will race at 11:00 am.')
+} else {
+  console.log('Your race number is ' + raceNumber + ' and you will race at 12:30 pm.' )
+}
+
